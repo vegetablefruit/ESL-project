dad27ae118ba3b2f8c77ecda0cf3afa9c1bb8691
vegetablefruit <vgruit972@gmail.com>
2025-11-17 10:53:11 +0000

    pwm

--- blinky/main.c
+++ blinky/main.c
@@ -6,7 +6,6 @@
 #include <stdbool.h>
 #include <nrfx_systick.h>
 
-// 7199
 #define BUTTON NRF_GPIO_PIN_MAP(1, 6)
 #define LED_1 NRF_GPIO_PIN_MAP(0, 6)
 #define LED_R NRF_GPIO_PIN_MAP(0, 8)
@@ -15,71 +14,92 @@
 
 // sequence
 uint32_t led_seq[] = {
-    LED_1, LED_1, LED_1, LED_1, LED_1, LED_1, LED_1,               // 7times
-    LED_R,                                                         // 1 times
-    LED_G, LED_G, LED_G, LED_G, LED_G, LED_G, LED_G, LED_G, LED_G, // 9 times
-    LED_B, LED_B, LED_B, LED_B, LED_B, LED_B, LED_B, LED_B, LED_B  // 9 times
-};
+    LED_1, LED_1, LED_1, LED_1, LED_1, LED_1, LED_1,
+    LED_R,
+    LED_G, LED_G, LED_G, LED_G, LED_G, LED_G, LED_G, LED_G, LED_G,
+    LED_B, LED_B, LED_B, LED_B, LED_B, LED_B, LED_B, LED_B, LED_B};
 #define SEQ_LENGTH (sizeof(led_seq) / sizeof(led_seq[0]))
 
 static uint32_t led_index = 0;
 static nrfx_pwm_t pwm0 = NRFX_PWM_INSTANCE(0);
 
-static uint16_t pwm_value = 0;  
+static uint16_t pwm_value = 0;
 static nrf_pwm_sequence_t pwm_seq = {
     .values.p_common = &pwm_value,
     .length = 1,
-    .repetition = 0,
-    .end_delay = 0
-}
+    .repeats = 0,
+    .end_delay = 0};
 
-void init_pwm(uint32_t pin){
+void pwm_init(uint32_t pin)
+{
     nrfx_pwm_config_t config = {
-        .output_pins = {
-            pin,
-            NRFX_PWM_PIN_NOT_USED,
-            NRFX_PWM_PIN_NOT_USED,
-            NRFX_PWM_PIN_NOT_USED
-        },
+        .output_pins = {pin, NRFX_PWM_PIN_NOT_USED, NRFX_PWM_PIN_NOT_USED, NRFX_PWM_PIN_NOT_USED},
         .irq_priority = NRFX_PWM_DEFAULT_CONFIG_IRQ_PRIORITY,
         .base_clock = NRF_PWM_CLK_1MHz,
         .count_mode = NRF_PWM_MODE_UP,
         .top_value = 1000,
         .load_mode = NRF_PWM_LOAD_COMMON,
-        .step_mode = NRF_PWM_STEP_AUTO
-    }
+        .step_mode = NRF_PWM_STEP_AUTO};
     nrfx_pwm_init(&pwm0, &config, NULL);
+
     nrfx_pwm_simple_playback(&pwm0, &pwm_seq, 1, NRFX_PWM_FLAG_LOOP);
 }
 
-//duty cycle
-void pwm_set_duty_cycle(uint16_t duty){
-    if (duty > 1000){
-        duty = 1000;
-        pwm_value = duty;
-    }
+void pwm_deinit_safe(void)
+{
+
+#if defined(NRFX_PWM_HAS_STOP) || defined(NRFX_PWM_STOP)
+    nrfx_pwm_stop(&pwm0, false);
+#else
+
+#endif
+    nrfx_pwm_uninit(&pwm0);
 }
 
-void pwm_switch_led(uint32_t pin){
-    nrfx_pwm_uninit(&pwm0);
+void pwm_set_duty(uint16_t duty)
+{
+    if (duty > 1000)
+        duty = 1000;
+    pwm_value = duty;
+}
+
+void pwm_switch_led(uint32_t pin)
+{
+
+    pwm_deinit_safe();
     pwm_init(pin);
 }
 
-//GPIOTE
-volatile uint32_t last_click_time = 0;
 volatile bool blinking = false;
 
-void button_handler(nrfx_gpiote_pin_t pin, nrf_gpiote_polarity_t action){
-    uint32_t now = nrfx_systick_get();
+static nrfx_systick_state_t last_state;
+static bool last_state_valid = false;
 
-    if(now - last_click_time < 350){
-        blinking = !blinking;
+#define DOUBLE_CLICK_MS 350
+#define DOUBLE_CLICK_US (DOUBLE_CLICK_MS * 1000U)
+
+void button_handler(nrfx_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
+{
+    nrfx_systick_state_t now;
+    nrfx_systick_get(&now);
+
+    if (last_state_valid)
+    {
+
+        bool elapsed_ge = nrfx_systick_test(&last_state, DOUBLE_CLICK_US);
+
+        if (!elapsed_ge)
+        {
+            blinking = !blinking;
+        }
     }
-    last_click_time = now;
 
+    last_state = now;
+    last_state_valid = true;
 }
 
-void gpiote_init(){
+void gpiote_init()
+{
     nrfx_gpiote_init();
 
     nrfx_gpiote_in_config_t config = NRFX_GPIOTE_CONFIG_IN_SENSE_TOGGLE(true);
@@ -89,11 +109,26 @@ void gpiote_init(){
     nrfx_gpiote_in_event_enable(BUTTON, true);
 }
 
-int main(void){
+static void startup_blink(uint32_t pin)
+{
+    nrf_gpio_cfg_output(pin);
+    nrf_gpio_pin_write(pin, 1);
+    nrf_delay_ms(80);
+    nrf_gpio_pin_write(pin, 0);
+    nrf_delay_ms(80);
+    nrf_gpio_pin_write(pin, 1);
+    nrf_delay_ms(80);
+    nrf_gpio_pin_write(pin, 0);
+}
+
+int main(void)
+{
     nrfx_systick_init();
     gpiote_init();
 
-    pwm_init(led_seq[0]);  // первый LED
+    startup_blink(LED_1);
+
+    pwm_init(led_seq[0]);
 
     uint16_t duty = 0;
     int direction = 1;
@@ -103,21 +138,18 @@ int main(void){
         if (blinking)
         {
             pwm_set_duty(duty);
-
-            duty += direction * 10;
-
+            duty = duty + direction * 10;
             if (duty >= 1000)
             {
+                duty = 1000;
                 direction = -1;
                 led_index = (led_index + 1) % SEQ_LENGTH;
                 pwm_switch_led(led_seq[led_index]);
             }
-
             if (duty == 0)
             {
                 direction = 1;
             }
-
             nrf_delay_ms(10);
         }
         else
